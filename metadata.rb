5e6c89a6caf1edf13e1d527d21a247162d973a06
Florin STAN <tom@tcnp3.com>
2014-08-12 11:52:07 -0600

    release version is 0.2.1

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'florin.stan@gmail.com'
 license          'Apache 2.0'
 description      'LWRPs for managing cloud resources using fog [http://fog.io]'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '0.2.1'
 
 depends 'build-essential'

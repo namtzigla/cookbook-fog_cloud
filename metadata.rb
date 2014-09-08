fd576138267086440c2270547cc428ceed8193a0
Florin STAN <florin.stan@gmail.com>
2014-09-08 10:42:44 -0600

    new dev version 0.4.0

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'florin.stan@gmail.com'
 license          'Apache 2.0'
 description      'LWRPs for managing cloud resources using fog [http://fog.io]'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3.0'
+version          '0.4.0'
 
 depends 'build-essential', '~> 2.0.6'
